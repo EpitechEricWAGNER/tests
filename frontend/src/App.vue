48a6a47d780e004bb197d1279f5a8e8a7bd7a2d8
Corentin Janson <janson.corentin@gmail.com>
2024-10-11 13:16:11 +0200

    Revert "Add Chadcn-Vue"
    This reverts commit 8f5dca504e95ec4900f5522c5f9c60c9f4f87b38.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,24 +1,7 @@
-<script setup lang="ts">
-import Dashboard from './components/Dashboard.vue'
+<script setup>
+import User from './components/User.vue';
 </script>
 
 <template>
-  <Dashboard />
+	<User />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
