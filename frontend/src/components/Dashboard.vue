cb16fa0426b0c84d580a16bea1bd9a0369f34f1f
Corentin Janson <janson.corentin@gmail.com>
2024-10-11 17:04:21 +0200

    Fix DockerFrontend Build

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -43,7 +43,7 @@ import {
     <div class="flex-1 space-y-4 p-8 pt-6">
       <div class="flex items-center justify-between space-y-2">
         <h2 class="text-3xl font-bold tracking-tight">
-          Dashboard
+          Greg on lui mange le uc
         </h2>
         <div class="flex items-center space-x-2">
           <DateRangePicker />
